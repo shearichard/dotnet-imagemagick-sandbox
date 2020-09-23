88d525ba9a378c763da6a52299161e96860d4fb4
rshea <rshea@thecubagroup.com>
2020-09-23 15:53:49 +1200

    Create a 'Country' model

---
+++ WebApplication1/WebApplication1/Models/Country.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Models
+{
+    /// <summary>
+    /// A class loosely based on the ISO 3166 standard
+    /// for identifying Countries
+    /// </summary>
+    public class Country
+    {
+        string Name_Short { get; set; }
+        string Code_Alpha_2 { get; set; }
+
+        public Country() { }
+
+        public Country(string shortName, string alpha2Code)
+        {
+            this.Name_Short = shortName;
+            this.Code_Alpha_2 = alpha2Code;
+        }
+    }
+}
